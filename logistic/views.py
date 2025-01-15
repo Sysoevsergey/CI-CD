9ea7b832c8bfeb8a311871fff493ab2bcdbdf950
“Admin” <sysoevserega93@gmail.com>
2025-01-15 22:31:54 +0300

    fix timer

--- logistic/views.py
+++ logistic/views.py
@@ -2,6 +2,7 @@ import datetime
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.filters import SearchFilter
 from rest_framework.viewsets import ModelViewSet
+from django.http import HttpResponse
 
 from logistic.models import Product, Stock
 from logistic.serializers import ProductSerializer, StockSerializer
@@ -20,9 +21,8 @@ class StockViewSet(ModelViewSet):
     filter_backends = [DjangoFilterBackend]
     filterset_fields = ['address', 'products']
 
+
 def time_view(request):
     current_time = datetime.datetime.now().time()
     msg = f'Текущее время: {current_time}'
     return HttpResponse(msg)
-
-
